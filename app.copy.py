127a16c8542db705d7369c4004c929647cecb22d
geonho <konho7@gmail.com>
2026-08-06 18:43:05 +0900

    update

--- app.copy.py
+++ app.copy.py
@@ -26,7 +26,7 @@ col3.metric(label="전체 퇴직률", value=f"{overall_rate:.1f}%")
 # 그래프 1 : 부서별 퇴직자 수
 st.subheader("부서별 퇴직자 수")
 fig1, ax1 = plt.subplots(figsize=(7, 3))
-attrition_df = df[df["attrition"] == "Yes"]
+attrition_df = df[df["attrition"] == "퇴직"]
 sns.countplot(data=attrition_df, x="department", ax=ax1)
 ax1.set_xlabel("부서")
 ax1.set_ylabel("퇴직자 수(명)")
